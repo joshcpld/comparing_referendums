bcbf7c752d4b704ec7fd470da0b0c2090d7bbe90
joshcpld <joshcpld@gmail.com>
2023-12-10 20:55:07 +1100

    Produced code which retrieves fist preference voting data from 2022 federal election.

--- 04_election_data.R
+++ 04_election_data.R
@@ -7,10 +7,11 @@ library(glue)
 ###################### Web-scraping electorate election data ###################
 ################################################################################
 
-########importing electorate names and stat pairings############################
+########importing electorate names and state pairings###########################
 
 electorate_names <- read_csv("data/voice_referendum_electorate_results.csv") %>% 
-  select(electorate, state)
+  select(electorate, state) %>% 
+  filter(electorate != "Total")
  
 
 ####### Figuring out which electorates correspond with which link numbers ######
@@ -49,70 +50,95 @@ electorate_webpage_pairings <- electorate_webpage_pairings %>%
 
 ####### scraping first preference count electorate election data ###############
 
-link <- "https://results.aec.gov.au/27966/Website/HouseDivisionPage-27966-197.htm"
+generate_url <- function(number) {
+  glue("https://results.aec.gov.au/27966/Website/HouseDivisionPage-27966-{number}.htm")
+}
 
-page <- read_html(link)
-
-column_numbers <- seq(1,  6, by = 1)
-
+column_numbers <- seq(1, 6, by = 1)
 column_names <- c("candidate", "party", "votes", "per_cent", "swing_per_cent", "status")
 
-retrieve_electorate_election_data <- function(column_number){
+retrieve_electorate_election_data <- function(link, number) {
+  page <- read_html(link)
   
-  html_code <- glue("#fp tbody td:nth-child({column_number})")
+  data <- map_dfc(column_numbers, function(column_number) {
+    html_code <- glue("#fp tbody td:nth-child({column_number})")
+    column_data <- page %>% 
+      html_nodes(html_code) %>% 
+      html_text()
+    return(column_data)
+  })
   
-  column_data <- page %>% 
-    html_nodes(html_code) %>% 
-    html_text()
+  colnames(data) <- column_names
+  data$number <- number  
   
-  return(column_data)
-  
+  return(data)
 }
 
-aston_results <- map(column_numbers, retrieve_electorate_election_data)
-
-aston_results <- as.tibble(do.call(cbind, aston_results)) %>% 
-  set_names(column_names) %>% 
-  mutate(electorate = "Aston")
-
-
-
-
-data <- page %>% 
-  html_nodes("#StandardHeading") %>% 
-  html_text()
-
-### first I need to webscrape relationships between names and link numbers before actually
-#getting results
-
-
-link_numbers <- seq(1,400, by = 1)
-
-retrieve_election_electorate_webpage_pairing <- function(link_number) {
-  link <- glue("https://results.aec.gov.au/27966/Website/HouseDivisionPage-27966-{link_number}.htm")
-  
-  # Try to read the HTML content, handle errors
-  result <- tryCatch(
-    {
-      page <- read_html(link)
-      data <- page %>% 
-        html_nodes("#StandardHeading") %>% 
-        html_text()
-      return(list(link_number = link_number, data = data))
-    },
-    error = function(e) {
-      cat("Error accessing:", link, "\n")
-      # Print or handle the error as needed
-      return(NULL)
-    }
-  )
-  
-  return(result)
+scrape_election_electorate_urls <- function(numbers) {
+  links_vector <- map(numbers, ~generate_url(.x))
+  result_df <- map2_dfr(links_vector, numbers, retrieve_electorate_election_data)
+  return(result_df)
 }
 
-electorate_webpage_pairings <- map(link_numbers,retrieve_election_electorate_webpage_pairing)
+electorate_first_preferences_votes <- scrape_election_electorate_urls(electorate_webpage_pairings$link_number)
 
-electorate_webpage_pairings <- electorate_webpage_pairings %>% 
-  bind_rows() %>% 
-  separate(data, into = c("electorate", "state"), sep = ", ", remove = FALSE) %>% 
-  select(-data)
+
+
+################################################################################
+###################### Cleaning electorate election data #######################
+################################################################################
+
+electorate_election_data <- electorate_first_preferences_votes %>% 
+  select(party,per_cent,link_number = number) %>% 
+  inner_join(electorate_webpage_pairings, by = "link_number") %>% 
+  select(electorate,state,party,per_cent) %>% 
+  mutate_at(vars(one_of("per_cent")), ~str_replace_all(.,",", "")) %>% 
+  mutate_at(vars(one_of("per_cent")), as.numeric) 
+
+
+################## Splitting up parties into more succinct groups ##############
+
+electorate_election_data <- electorate_election_data %>% 
+  mutate(party_simple = case_when(
+    
+    str_detect(party, "Labor") ~ "Labor",
+    party == "A.L.P." ~ "Labor",
+    party %in% c("Liberal", "Liberal National Party of Queensland", "National Party", 
+                 "National Party of Australia", "National Party of Australia - N.S.W.",
+                 "The Nationals") ~ "LNP",
+    party %in% c("The Greens", "Queensland Greens", "The Greens (WA)") ~ "Greens",
+    TRUE ~ "independent"
+    )) %>% 
+  select(electorate,state,party,party_simple,per_cent) %>% 
+  group_by(electorate,party_simple) %>% 
+  summarise(per_cent = sum(per_cent))
+
+write_csv(electorate_election_data, "data/electorate_election_data.csv")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
